0535e3ab51a2a04189f8da52f83265f66cc368f2
Kevin Greene <kevin.greene@creditkarma.com>
2018-03-20 10:58:38 -0700

    fix: Fix handling of parent asyncId

--- src/main/State.ts
+++ src/main/State.ts
@@ -2,6 +2,7 @@ export class State {
     constructor(
         public enabled: boolean = true,
         public previousIds: Array<number> = [],
+        public previousParents: Array<number> = [],
         public nextId: number = 0,
         public currentId: number = 0,
         public parentId: number = 0,
